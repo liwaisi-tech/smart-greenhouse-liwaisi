e0cc629b734b56bddb1f98bf09f9a8922eb786d9
Ladybataca <btatacc@gmail.com>
2024-09-30 21:02:14 -0500

    se sube estructura de datos para sensado

---
+++ components/tempHumidity/include/tempHumidity.h
@@ -0,0 +1,16 @@
+#ifndef TEMPHUMIDITY_H
+#define TEMPHUMIDITY_H
+#include <freertos/FreeRTOS.h>
+
+typedef struct
+{
+    float temperature1;
+    float humidity1;
+    float temperature2;
+    float humidity2;
+    float: humGroud1:
+    float humGround2;
+} TempHumidity;
+
+
+#endif // TEMPHUMIDITY_H
---
+++ components/tempHumidity/tempHumidity.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "tempHumidity.h"
+
+void func(void)
+{
+
+}
