d54aa8286860026058625a9d41389b5a63a2e130
Ladybataca <btatacc@gmail.com>
2024-12-17 17:52:51 -0500

    stage de cambios iniciales en actionpage

--- components/actionPage/actionPage.c
+++ components/actionPage/actionPage.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 #include "actionPage.h"
+#include "tempHumidity.h"
+#include "sensorYL69.h"
+#include "yl69.h"
+#include "esp_log.h"
 
-void func(void)
-{
-/* void get_data_YL69_task(void *pvParameters) {
+tempHumidity_t receive_data_YL69_task(void *arg){
+    action_config_t; *config = (action_config_t *) arg;
+    tempHumidity_t data;
     yl69_reading_t reading;
-    tempHumidity_t data = {0};
-    
-    while (1) {
-        ESP_LOGI(TAG, "YL69sensor leyendo...");
-        // Leer datos de ambos sensores
-        if (xQueueReceive(sensor_configs[0].queue, &reading, portMAX_DELAY)) {
-            if (reading.sensor_id == 1) {
-                data.humGroud1 = reading.humidity;
-                ESP_LOGI(TAG, "Humedad tierra cajón 1: %f", data.humGroud1);
-            } else if (reading.sensor_id == 2) {
-                data.humGroud2 = reading.humidity;
-                ESP_LOGI(TAG, "Humedad tierra cajón 2: %f", data.humGroud2);
-            }
+    if (xQueueReceive(config.queue, &reading, 0) == pdTRUE) {
+        if (reading.sensor_id ==1){
+            data.humGroud1 = reading.humidity;
         }
         else{
-            ESP_LOGE(TAG, "No se recibio datos de YL69");
+            data.humGroud2 = reading.humidity;
         }
-        
-        // Aquí puedes agregar código para enviar data a otra cola si es necesario
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Pequeña pausa para evitar sobrecarga
-    } */
+       
+        ESP_LOGI(TAG, "Dato recibido humedad suelo cajon %d = %d", reading.sensor_id, reading.humidity);      
+    }
+        // Esta es para afuera cuando el handler llame, cada segundo.
+       // vTaskDelay(pdMS_TO_TICKS(1000)); // Ajusta el intervalo de lectura según sea necesario
 }
--- components/actionPage/include/actionPage.h
+++ components/actionPage/include/actionPage.h
@@ -1,1 +1,15 @@
-void func(void);
+#ifndef  ACTIONPAGE_H
+#define ACTIONPAGE_H
+#include "esp_err.h"
+
+typedef struct {
+    int pin;
+    QueueHandle_t queue;
+    int sensor_type;
+} action_config_t;
+
+void action_page_main (QueueHandle_t *buffer);
+tempHumidity_t receive_data_YL69_task(void *arg);
+tempHumidity_t receive_data_DHT_task (void *pvParameter);
+
+#endif 
